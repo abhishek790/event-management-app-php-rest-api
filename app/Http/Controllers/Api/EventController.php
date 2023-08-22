1e5100a7ad4cc8c9a2f911ed2c4c45df05967378
Abhishek Mishra <mishraavi790@gmail.com>
2023-08-22 17:12:41 +0545

    loading

--- app/Http/Controllers/Api/EventController.php
+++ app/Http/Controllers/Api/EventController.php
@@ -9,21 +9,14 @@ use \App\Models\Event;
 
 class EventController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
-    public function index()
-    { //we will modify the index action so the resource class can be used both to return a collection of specific resources where every element on this collection will be converted using this toarray() method or just one single resource
 
-        // now you would use this resource class for collection by typing the event resource,it has a static collection method to which you will just pass an array of resources which is a result of Event::all()
-        return EventResource::collection(Event::with('user', 'attendees')->get());
-        // api resources allow you to have more fields to add some kind to meta fields
-        //note: yo garda data haru json bhane field ma wrap hunxa { "data":[] }
+    public function index()
+    {
+        return EventResource::collection(Event::with('user', 'attendees')->paginate());
+
     }
 
-    /**
-     * Store a newly created resource in storage.
-     */
+
     public function store(Request $request)
     {
         $event = Event::create([
@@ -39,19 +32,14 @@ class EventController extends Controller
         return new EventResource($event);
     }
 
-    /**
-     * Display the specified resource.
-     */
+
     public function show(Event $event)
-    { // to return single request you would use this event resource,but this time creating a new instance of it
-        // if you already have event then you can do this to load user
+    {
         $event->load('user', 'attendees');
         return new EventResource($event);
     }
 
-    /**
-     * Update the specified resource in storage.
-     */
+
     public function update(Request $request, Event $event)
     {
 
@@ -72,9 +60,7 @@ class EventController extends Controller
 
     }
 
-    /**
-     * Remove the specified resource from storage.
-     */
+
     public function destroy(Event $event)
     {
         $event->delete();
--- app/Http/Resources/EventResource.php
+++ app/Http/Resources/EventResource.php
@@ -15,23 +15,19 @@ class EventResource extends JsonResource
      * @return array<string, mixed>
      */
 
-    // API resources is a way to customize how your data from your eloquent models is being transformed to json responses ,now laravel can convert eloquent models to json without using the api resources.By default, it would use a toJson() method which is on every single eloquent model but to have more control we use api resource
+
     public function toArray(Request $request): array
-    { //here we implement how specific resource should be converted to json
-        // return parent::toArray($request);
+    {
 
         return [
-            // lets return an array,we can customize the attribute names of whatever is being returned from the API,so the json returned does not need to be identical to what we have inside the eloquent model,now you have got all the model data inside this,so you can use this ID and this would be matched 1 to 1 to the attributes from the model
+
             'id' => $this->id,
             'name' => $this->name,
             'description' => $this->description,
             'start_time' => $this->start_time,
             'end_time' => $this->end_time,
 
-            // every event has this user_id column which points to the owner of a specific event(organizer), so you when getting all the events you might want to display also the organizer name, if you have 100 or 200 events returned at once, that would be inefficient to fetch all the events and then try to make let's say 100 request for every single user just to display the organizer of the event, so we can build some nested resources inside such event resource by conditionally loading the user
 
-            // we add an optional field called user, but we won't be passing the user that's associated with this event instead we will use the magical method of the resource called whenloaded() and pass the name of the relationship(it is between Event model and User model)
-            // what happens here is the user property would only be present on the response if this user relationship of particular event is loaded for eg: EventResource::collection(Event::with('user')->get());
             'user' => new UserResource($this->whenLoaded('user')),
             'attendee' => AttendeeResource::collection(
                 $this->whenLoaded('attendees')
@@ -40,5 +36,5 @@ class EventResource extends JsonResource
         ];
 
     }
-    // this class is not directly tied to the event model or event controller,so we will have to use those resource classes explicitly,we can use it for an event model and moreover you can have many resource classes for one single model in case you would like to serialize it to,different kind of outputs
+
 }
